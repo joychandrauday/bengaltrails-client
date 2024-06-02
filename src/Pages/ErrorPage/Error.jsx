bd386c81b1eda42628d849a94ae67bcbcf854445
joychandrauday <joychandraud@gmail.com>
2024-06-02 14:06:33 +0600

    set up the footer  and its design

--- src/Pages/ErrorPage/Error.jsx
+++ src/Pages/ErrorPage/Error.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-    return (
-        <div style={{
-            backgroundImage:"url('https://i.ibb.co/qBFwhK0/eugenio-mazzone-6ywyo2qta-Z8-unsplash.jpg')",
-            backgroundPosition:'center center',
-            backgroundSize:'cover'
-        }} className='min-h-screen flex items-center justify-center '>
-            <Helmet>
-                <title>Error | page not found.</title>
-            </Helmet>
-            <div className="wrapper backdrop-blur-lg p-24 rounded-lg">
-                <img src="https://i.ibb.co/McLzr4x/2.gif" alt="" className='w-44 mx-auto rounded '/>
-                <h1 className="capitalize font-bold text-4xl text-white py-3">Error 404 | page not found.</h1>
-                <Link to={"/"} className="btn bg-basic capitalize font-bold rounded-none w-full">back to home</Link>
-            </div>
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "url('https://i.ibb.co/St2K5g6/drew-murphy-P2-M1-Nj9-YJfo-unsplash.jpg')",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+      }}
+      className="min-h-screen flex items-start p-12 justify-start "
+    >
+      <Helmet>
+        <title>Error | page not found.</title>
+      </Helmet>
+      <div className="wrapper bg-primary p-4 rounded" >
+        <div className="">
+          <img
+            src="https://i.ibb.co/J2SjmV8/output-onlinegiftools.gif"
+            alt=""
+            className="w-44 mx-auto rounded "
+          />
         </div>
-    );
+        <h1 className="capitalize font-extrabold text-3xl text-white py-3">
+          Error 404 | destination not found.
+        </h1>
+        <Link
+          to={"/"}
+          className="btn bg-primary shadow-2xl shadow-black border-none text-white capitalize font-bold rounded-none w-full"
+        >
+          back to home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
